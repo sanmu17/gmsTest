27fc960a96873e248352953b402e3f71ac9732c8
15374 <1537414333@qq.com>
2021-08-04 10:14:27 +0800

    datastuct-sparseArr

--- DataStruct/src/com/data/SparseArr.java
+++ DataStruct/src/com/data/SparseArr.java
@@ -9,8 +9,60 @@ public class SparseArr {
         int chessArr[][]= new int[11][11];
         chessArr[1][2]=1;
         chessArr[2][3]=2;
+        chessArr[5][7]=8;
         System.out.println("原始的二维数组--------");
         for (int[] rows : chessArr) {
+            for (int data : rows) {
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+
+        /**
+         * 二位数组转稀疏数组
+         */
+        int num=0;
+        for (int i = 0; i < chessArr.length ; i++) {
+            for (int j = 0; j < 11; j++) {
+                if(chessArr[i][j]!=0){
+                    num++;
+                }
+            }
+        }
+        int sparseArr[][]=new int[num+1][3];
+        sparseArr[0][0]=11;
+        sparseArr[0][1]=11;
+        sparseArr[0][2]=num;
+
+        int count=0;
+        for (int i = 0; i < chessArr.length ; i++) {
+            for (int j = 0; j < 11; j++) {
+                if(chessArr[i][j]!=0){
+                     count++;
+                     sparseArr[count][0]=i;
+                     sparseArr[count][1]=j;
+                     sparseArr[count][2]=chessArr[i][j];
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("稀疏数组-----");
+        for (int[] row : sparseArr) {
+            System.out.printf("%d\t%d\t%d\t\n",row[0],row[1],row[2]);
+        }
+        System.out.println();
+
+        /**
+         * 稀疏数组转二位数组
+         */
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        //遍历系稀疏数组，把数据放入二维数组对应位置
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]]=sparseArr[i][2];
+        }
+
+        System.out.println("稀疏数组转二维数组--------");
+        for (int[] rows : chessArr2) {
             for (int data : rows) {
                 System.out.printf("%d\t",data);
             }
