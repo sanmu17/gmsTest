20cb7d5b78866b705a723836478508902297d34d
garments <1537414333@qq.com>
2024-03-02 10:27:32 +0800

    fix

--- DataStruct/src/com/data/SparseArr.java
+++ DataStruct/src/com/data/SparseArr.java
@@ -23,8 +23,36 @@ public class SparseArr {
         }
 
         /**
-         * 二位数组转稀疏数组
+         * 二维数组转稀疏数组
          */
+        int[][] sparseArr = getSparseArr(chessArr);
+        System.out.println();
+        System.out.println("稀疏数组-----");
+        for (int[] row : sparseArr) {
+            System.out.printf("%d\t%d\t%d\t\n",row[0],row[1],row[2]);
+        }
+        System.out.println();
+        /**
+         * 将稀疏数组存放到磁盘上
+         */
+        saveSpareArrToSSD(sparseArr);
+
+        /**
+         * 稀疏数组转二维数组
+         */
+        spareArrToNormalArr(sparseArr);
+    }
+
+    /**
+     * 二维数组转稀疏数组
+     * @param chessArr
+     * @return 稀疏数组
+     * 稀疏数组 （m,3）m行3列:
+     *                      第一行：二维数组的行数，列数，（不为0）值的个数（即m-1）
+     *                      第二行：第一个值的行坐标，列坐标，值
+     *                      第三行：与第二行类似，第二个值的行列坐标，值（以此类推，直到第m个值）
+     */
+    private static int[][] getSparseArr(int[][] chessArr) {
         int num=0;
         for (int i = 0; i < chessArr.length ; i++) {
             for (int j = 0; j < 11; j++) {
@@ -45,19 +73,39 @@ public class SparseArr {
                      count++;
                      sparseArr[count][0]=i;
                      sparseArr[count][1]=j;
-                     sparseArr[count][2]=chessArr[i][j];
+                     sparseArr[count][2]= chessArr[i][j];
                 }
             }
         }
-        System.out.println();
-        System.out.println("稀疏数组-----");
-        for (int[] row : sparseArr) {
-            System.out.printf("%d\t%d\t%d\t\n",row[0],row[1],row[2]);
+        return sparseArr;
+    }
+
+    /**
+     * 疏数组转二维数组
+     * @param sparseArr
+     */
+    private static void spareArrToNormalArr(int[][] sparseArr) {
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        //遍历系稀疏数组，把数据放入二维数组对应位置
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]]= sparseArr[i][2];
         }
-        System.out.println();
-        /**
-         * 将稀疏数组存放到磁盘上
-         */
+
+        System.out.println("稀疏数组转二维数组--------");
+        for (int[] rows : chessArr2) {
+            for (int data : rows) {
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+    }
+
+    /***
+     *
+     * @param sparseArr  稀疏数组
+     *    将稀疏数组存放到磁盘上
+     */
+    private static void saveSpareArrToSSD(int[][] sparseArr) {
         FileWriter writer=null;
         try {
             File file = new File("data.txt");
@@ -76,24 +124,6 @@ public class SparseArr {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-
-        /**
-         * 稀疏数组转二位数组
-         */
-        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
-        //遍历系稀疏数组，把数据放入二维数组对应位置
-        for (int i = 1; i < sparseArr.length; i++) {
-            chessArr2[sparseArr[i][0]][sparseArr[i][1]]=sparseArr[i][2];
-        }
-
-        System.out.println("稀疏数组转二维数组--------");
-        for (int[] rows : chessArr2) {
-            for (int data : rows) {
-                System.out.printf("%d\t",data);
-            }
-            System.out.println();
         }
     }
 }
